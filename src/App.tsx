3be4e848fb6026dca73226a5d05bed8c01805deb
unknown <cauechavesvb@gmail.com>
2023-03-17 16:15:59 -0300

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -40,21 +40,21 @@ function App() {
   return (
     <div className="flex justify-center items-start w-screen h-screen bg-gradient-to-t from-yellow-400 to-orange-500">
       <div className="flex flex-col w-full sm:w-auto">
-        <h1 className="sm:mt-40 text-teal-50 text-5xl sm:text-6xl mx-auto">
+        <h1 className="sm:mt-40  text-yellow-100 text-5xl sm:text-6xl mx-auto">
           Weather Forecast
         </h1>
         {showCard && <Card wD={weatherData} />}
-        <div className="bg-white mt-10 rounded">
+        <div className="bg-yellow-200 mt-10 rounded">
           <form onSubmit={handleSubmit}>
             <input
-              className="text-xl px-4 py-3 w-11/12 "
+              className="text-xl bg-yellow-200  px-4 rounded py-3 w-11/12 "
               placeholder="Insira aqui o nome da cidade"
               value={inputValue}
               onChange={handleInputChange}
             ></input>
 
             <button className="pl-2">
-              <SearchIcon />
+              <SearchIcon className="text-slate-400" />
             </button>
           </form>
         </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 export function Card({ wD }: any) {
   return (
-    <div className="flex flex-col gap-4 pl-12 py-4 sm:mt-10 h-72 w-full bg-white rounded-lg">
+    <div className="flex flex-col gap-4 pl-12 py-4 sm:mt-10 h-72 w-full bg-slate-100 rounded-lg">
       <div className="flex justify-between">
         <div className="text-lg">
           {wD.location.name}, {wD.location.country}, {wD.location.region}
@@ -41,60 +41,21 @@ export function Card({ wD }: any) {
           <span className="font-bold">{wD.current.humidity}%</span>
         </div>
       </div>
+
       <div className="h-[1px] w-11/12 bg-orange-300 text-center"></div>
+
       <div className="flex justify-around pr-12 gap-3">
-        <div className="text-center">
-          <div className="font-bold">{wD.location.localtime.slice(5, 10)}</div>
-          <span className="text-orange-400 font-bold">
-            {wD.forecast.forecastday[1].day.mintemp_c}°{" "}
-            {wD.forecast.forecastday[1].day.maxtemp_c}°
-          </span>
-        </div>
-        <div className="text-center">
-          <div className="font-bold">
-            {wD.forecast.forecastday[1].date.slice(5, 10)}
+        {wD.forecast.forecastday.map((item: any) => (
+          <div key={item.date.slice(5, 10)} className="text-center">
+            <div className="font-bold">{item.date.slice(5, 10)}</div>
+            <span className="text-orange-400 font-bold">
+              <ArrowUpwardIcon className="text-orange-300" />{" "}
+              {item.day.mintemp_c}°{" "}
+              <ArrowDownwardIcon className="text-orange-300" />
+              {item.day.maxtemp_c}°
+            </span>
           </div>
-          <span className="text-orange-400 font-bold">
-            {wD.forecast.forecastday[2].day.mintemp_c}°{" "}
-            {wD.forecast.forecastday[2].day.maxtemp_c}°
-          </span>
-        </div>
-        <div className="text-center">
-          <div className="font-bold">
-            {wD.forecast.forecastday[2].date.slice(5, 10)}
-          </div>
-          <span className="text-orange-400 font-bold">
-            {wD.forecast.forecastday[3].day.mintemp_c}°{" "}
-            {wD.forecast.forecastday[3].day.maxtemp_c}°
-          </span>
-        </div>
-        <div className="text-center">
-          <div className="font-bold">
-            {wD.forecast.forecastday[3].date.slice(5, 10)}
-          </div>
-          <span className="text-orange-400 font-bold">
-            {wD.forecast.forecastday[1].day.mintemp_c}°{" "}
-            {wD.forecast.forecastday[1].day.maxtemp_c}°
-          </span>
-        </div>
-        <div className="text-center">
-          <div className="font-bold">
-            {wD.forecast.forecastday[4].date.slice(5, 10)}
-          </div>
-          <span className="text-orange-400 font-bold">
-            {wD.forecast.forecastday[4].day.mintemp_c}°{" "}
-            {wD.forecast.forecastday[4].day.maxtemp_c}°
-          </span>
-        </div>
-        <div className="text-center">
-          <div className="font-bold">
-            {wD.forecast.forecastday[5].date.slice(5, 10)}
-          </div>
-          <span className="text-orange-400 font-bold">
-            {wD.forecast.forecastday[5].day.mintemp_c}°{" "}
-            {wD.forecast.forecastday[5].day.maxtemp_c}°
-          </span>
-        </div>
+        ))}
       </div>
     </div>
   );
